f23028d1beda4365c75e1ea8e5a0b3c6ea4f7ff7
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-07 22:07:18 +0600

    oop_connection

--- database.php
+++ database.php
@@ -4,10 +4,15 @@ class Database
     public $host = "DB_HOST";
     public $user = "DB_USER";
     public $pass = "DB_PASS";
-    public $name = "DB_NAME";
+    public $dbname = "DB_NAME";
 
     public $link;
     public $error;
+
+    private function connectDB()
+    {
+        $this->link = new mysqli($this->host, $this->user, $this->pass, $this->dbname);
+    }
 }
 
 
