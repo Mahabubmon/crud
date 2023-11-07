42a01633339db85cfe9ce042cc24ad2df44011f8
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-07 22:02:55 +0600

    database_connection

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+class Database
+{
+    public $host = "DB_HOST";
+    public $user = "DB_USER";
+    public $pass = "DB_PASS";
+    public $name = "DB_NAME";
+
+    public $link;
+    public $error;
+}
+
+
+?>
