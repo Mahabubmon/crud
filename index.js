253a152b025396fc5167c66b8a0ef8d19eb4cceb
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-10 11:55:41 +0600

    js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-new DataTable("#example");
+$(document).ready(function () {
+  new DataTable("#example");
+
+  function showAllUsers() {
+    $.ajax({
+      url: "action.php",
+      type: "POST",
+      data: { action: "viwe" },
+      successe: function (response) {
+        console.log(response);
+      },
+    });
+  }
+});
