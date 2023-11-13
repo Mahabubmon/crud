473f7c1f18557aa2ee383563ae95c1e6b4f94349
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-13 23:19:17 +0600

    modal_adding

--- index.php
+++ index.php
@@ -14,6 +14,24 @@
 </head>
 
 <body>
+
+  <!-- //Add New User Modal -->
+  <div class="modal" tabindex="-1" id="addNewUserModal">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Add New User</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>Modal body text goes here.</p>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- //Add New User Modal end -->
   <div class="container">
     <div class="row mt-4">
       <div class="col-lg-12 d-flex justify-content-between align-items-center">
@@ -29,7 +47,7 @@
     <div class="row">
       <div class="col-lg-12">
         <div class="table-responsive">
-          <table class="table table-striped table-border text-center">
+          <table class="table table-striped table-bordered text-center">
             <thead>
               <tr>
                 <th>ID</th>
@@ -40,6 +58,19 @@
                 <th>Acion</th>
               </tr>
             </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Rahul</td>
+                <td>Ataullah</td>
+                <td>rahul@test.com</td>
+                <td>01234567899</td>
+                <td>
+                  <a href="" class="btn btn-success btn-sm rounded-pill py-0">Edit</a>
+                  <a href="" class="btn btn-danger btn-sm rounded-pill py-0">Delete</a>
+                </td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
