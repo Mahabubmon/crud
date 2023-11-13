2e16766b43db375ed00ed3df88f15ddcf2948532
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-13 22:55:04 +0600

    data table

--- index.php
+++ index.php
@@ -70,6 +70,32 @@
     </div>
   </div>
 
+  <div class="container">
+    <div class="row">
+      <table id="example" class="table table-striped" style="width:100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Age</th>
+            <th>Start date</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Tiger Nixon</td>
+            <td>System Architect</td>
+            <td>Edinburgh</td>
+            <td>61</td>
+            <td>2011-04-25</td>
+            <td>$320,800</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
   <!-- Add New user  Modal -->
   <div class="modal fade" id="addModal">
     <div class="modal-dialog modal-dialog-centered">
