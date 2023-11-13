5693e8a47f5a8432364e13c7fe7ff793be700f31
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-13 23:10:25 +0600

    table_adding

--- index.php
+++ index.php
@@ -15,13 +15,32 @@
 
 <body>
   <div class="container">
-    <div class="row">
-      <div class="col-lg-12">
+    <div class="row mt-4">
+      <div class="col-lg-12 d-flex justify-content-between align-items-center">
         <div>
           <h4 class="text-primary">All users in the database</h4>
         </div>
         <div>
           <button class="btn btn-primary">Add New User</button>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="table-responsive">
+          <table class="table table-striped table-border text-center">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>E-mail</th>
+                <th>Phone</th>
+                <th>Acion</th>
+              </tr>
+            </thead>
+          </table>
         </div>
       </div>
     </div>
