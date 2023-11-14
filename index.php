5ac5ffdd00c5327eb6bd6aebb3d29c7f07a69b4f
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-14 18:49:07 +0600

    modal_body

--- index.php
+++ index.php
@@ -24,8 +24,41 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <p>Modal body text goes here.</p>
+          <form action="" method="" id="add-user-form" class="p-2" novalidate>
+            <div class="row mb-3 gx-3">
+              <div class="col">
+                <input type="text" name="fname" id="fname" class="form-control
+                form-control-lg" placeholder="Enter First Name" required>
+                <div class="invalid-feedback">
+                  First name is required
+                </div>
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="col">
+                <input type="text" name="lname" id="lname" class="form-control
+                form-control-lg" placeholder="Enter Last Name" required>
+                <div class="invalid-feedback">
+                  Last name is required
+                </div>
+              </div>
+            </div>
+            <div class="mb-3">
+              <input type="email" name="email" id="email" class="form-control form-control-lg"
+                placeholder="Enter E-mail" required>
+              <div class="invalid-feedback">E-mail is required</div>
+            </div>
+            <div class="mb-3">
+              <input type="phone" name="phone" id="phone" class="form-control form-control-lg" placeholder="Enter Phone"
+                required>
+              <div class="invalid-feedback"> Phone is required</div>
+            </div>
+            <div class="mb-3">
+              <input type="submit" value="Add Users" class="btn btn-primary btn-block btn-lg" id="add-user-btn">
+            </div>
+          </form>
         </div>
+
       </div>
     </div>
   </div>
