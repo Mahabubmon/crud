0dcb97a8fa04b46e2b863a72fa01552b2753ca2b
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-15 23:18:03 +0600

    action

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const addForm = document.getElementById("add-user-form");
 
 //Add New User ajax Request
-addForm.addEventListener("submit", (e) => {
+addForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const formData = new FormData(addForm);
   formData.append("add", 1);
@@ -12,5 +12,13 @@ addForm.addEventListener("submit", (e) => {
     addForm.classList.add("was-validated");
     return false;
   } else {
+    document.getElementById("add-user-btn").value = "Please wait...";
+
+    const data = await fetch("action.php", {
+      method: "POST",
+      body: formData,
+    });
+    const response = await data.text();
+    console.log(response);
   }
 });
