5f1824d4f5371340fa5125fc45912cb9624f4db9
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-16 18:30:58 +0600

    re_start

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Ajax CRUD</h1>
+        <input type="text" placeholder="enter Name..."><br>
+        <input type="email" placeholder="enter Email..."><br>
+        <input type="text" placeholder="enter Password..."><br>
+
+    </div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="ajax.js"></script>
+
+</body>
+
+</html>
