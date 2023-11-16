f4ae4e41e6da7de895c7ade19cd9c40b844fbcbb
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-16 19:18:57 +0600

    Database-connection

---
+++ action.php
@@ -0,0 +1,17 @@
+<?php
+$connect = mysqli_connect("localhost", "root", "", "ajax");
+$name = $_POST['name'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+
+
+$insert = "INSERT INTO ajax (Name,Email,pass)
+VALUES($name,$email,$pass)";
+$query = mysqli_query($connect, $insert);
+if ($query) {
+    echo "Data Insert Successfully";
+} else {
+    echo "Data  Not Inserted";
+}
+
+?>
