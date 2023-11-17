f152cbda23cb50becd1443bb1a5e630040170dec
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-17 10:01:25 +0600

    edit_db_connection

--- edit.php
+++ edit.php
@@ -1,9 +1,17 @@
+<?php
+require_once 'db.php';
+
+$id = $_GET['userId'];
+$selete = "SELECT * FROM ajax WHERE id = '$id'";
+$ex = mysqli_query($connect, $select);
+$row = mysqli_fetch_array($ex);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
 </head>
